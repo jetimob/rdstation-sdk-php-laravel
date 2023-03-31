bad9a304be8ae05fc80147cf2d420116a7db6999
Alan Weingartner <hi@alanwgt.com>
2023-03-31 09:58:58 -0300

    fix: corrige obtenção do refresh token

--- src/Http/Authorization/OAuth/TokenResolver/RDStationTokenResolver.php
+++ src/Http/Authorization/OAuth/TokenResolver/RDStationTokenResolver.php
@@ -4,6 +4,7 @@ namespace Jetimob\RDStation\Http\Authorization\OAuth\TokenResolver;
 
 use GuzzleHttp\RequestOptions;
 use Jetimob\Http\Authorization\OAuth\AccessToken;
+use Jetimob\Http\Authorization\OAuth\Exceptions\AccessTokenExpiredException;
 use Jetimob\Http\Authorization\OAuth\Exceptions\AuthorizationCodeRequiredException;
 use Jetimob\Http\Authorization\OAuth\OAuthClient;
 use Jetimob\Http\Authorization\OAuth\OAuthFlow;
@@ -12,14 +13,31 @@ use Jetimob\Http\Authorization\OAuth\TokenResolvers\OAuthTokenResolver;
 class RDStationTokenResolver extends OAuthTokenResolver
 {
     /**
+     * @link https://developers.rdstation.com/reference/obtendo-token-de-acesso-pelo-refresh_token
      * @throws AuthorizationCodeRequiredException
      * @throws \JsonException
      */
     public function resolveAccessToken(OAuthClient $client, ?string $credentials = null): AccessToken
     {
         return $this->issueAccessTokenRequest($client, OAuthFlow::AUTHORIZATION_CODE, $credentials, function (array $requestOptions) use ($client) {
-            // precisamos mandar o client_id quando o token está sendo atualizado
-            // @link https://developers.rdstation.com/reference/obtendo-token-de-acesso-pelo-refresh_token
+            $requestOptions[RequestOptions::FORM_PARAMS]['client_secret'] = $client->getClientSecret();
+            return $requestOptions;
+        });
+    }
+
+    /**
+     * @link https://developers.rdstation.com/reference/obtendo-token-de-acesso-pelo-refresh_token
+     * @throws AccessTokenExpiredException
+     * @throws AuthorizationCodeRequiredException
+     * @throws \JsonException
+     */
+    public function refreshAccessToken(OAuthClient $client, AccessToken $accessToken): AccessToken
+    {
+        if (!$accessToken->hasRefreshToken()) {
+            throw new AccessTokenExpiredException('The access token has expired and cannot be refreshed!');
+        }
+
+        return $this->issueAccessTokenRequest($client, 'refresh_token', $accessToken->getRefreshToken(), function (array $requestOptions) use ($client) {
             $requestOptions[RequestOptions::FORM_PARAMS]['client_id'] = $client->getClientId();
             $requestOptions[RequestOptions::FORM_PARAMS]['client_secret'] = $client->getClientSecret();
             return $requestOptions;
